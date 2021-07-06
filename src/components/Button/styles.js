c2a0584c529ac23b32e4bf3768c7bd0d3f3823c3
jungleologyphong <71376863+jungleologyphong@users.noreply.github.com>
2021-07-06 16:34:35 +0700

    16:35 07/06, Nhat Phong : Commit VFTPhoneNumberScreen -  65%

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,7 +4,9 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   container: {
     flexDirection: 'row',
-    height: 45,
+    height: 60,
+    marginLeft: 16,
+    marginRight: 16,
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: getSize.m(20),
--- src/screens/Bottom/index.js
+++ src/screens/Bottom/index.js
@@ -1,5 +1,7 @@
 import HomeScreen from './HomeScreen';
+import VFTPhoneNumberScreen from './OTPScreen';
 
 export const bottom = {
   HOMESCREEN: HomeScreen,
+  VFTPhoneNumberScreen: VFTPhoneNumberScreen,
 };
