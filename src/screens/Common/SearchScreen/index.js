a6fcea841bd5d76c9177dbde079da331c327cb48
Hieu Ngan <=>
2021-09-03 18:05:52 +0700

    UI/SearchScreen

--- src/screens/Common/SearchScreen/index.js
+++ src/screens/Common/SearchScreen/index.js
@@ -1,10 +1,86 @@
-import {Block, Text, Header} from '@components';
-import React from 'react';
+import {Block, Text, Header, TextInput} from '@components';
+import React, {useState} from 'react';
+import {theme} from '@theme';
+import styles from './styles';
+import {Image, FlatList, Pressable} from 'react-native';
+import {icons} from '@assets';
+import ItemSearch from '@components/Common/ItemList/ItemSearch';
+
+const dataSearch = [
+  {
+    title: 'Cheese Burger',
+  },
+  {
+    title: 'Indian Pasta',
+  },
+  {
+    title: 'Beef Burger',
+  },
+  {
+    title: 'Salad',
+  },
+];
 
 const SearchScreen = () => {
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6]);
+  const [key, setKey] = useState('');
+  const _renderItem = ({item}) => <ItemSearch />;
+  const _renderItemSearch = ({item}) => (
+    <Block>
+      <Text fontType="bold" marginTop={12}>
+        {item.title}
+      </Text>
+    </Block>
+  );
+
   return (
-    <Block flex>
-      <Header title="Search" />
+    <Block flex backgroundColor={theme.colors.white}>
+      <Header title="Search" canGoBack colorTheme={theme.colors.black} />
+      <Block paddingHorizontal={16}>
+        <TextInput
+          placeholder="Search"
+          inputStyle={styles.inputStyle}
+          containerInputStyle={styles.containerInputStyle}
+          rightIcon={() => (
+            <Pressable>
+              <Image style={styles.iconSeach} source={icons.filter} />
+            </Pressable>
+          )}
+        />
+        {data.length > 0 ? (
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={data}
+            keyExtractor={item => item.id}
+            renderItem={_renderItem}
+          />
+        ) : (
+          <Block>
+            <Block>
+              <Text fontType="bold" color={theme.colors.blue}>
+                History
+              </Text>
+              <FlatList
+                showsVerticalScrollIndicator={false}
+                data={[1, 2]}
+                keyExtractor={item => item.id}
+                renderItem={_renderItem}
+              />
+            </Block>
+            <Block marginTop={20}>
+              <Text fontType="bold" color={theme.colors.blue}>
+                Recent search | Clear all
+              </Text>
+              <FlatList
+                showsVerticalScrollIndicator={false}
+                data={dataSearch}
+                keyExtractor={item => item.id}
+                renderItem={_renderItemSearch}
+              />
+            </Block>
+          </Block>
+        )}
+      </Block>
     </Block>
   );
 };
