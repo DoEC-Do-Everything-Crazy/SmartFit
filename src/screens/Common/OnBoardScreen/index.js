bfacd1fde92752f2968288f6d1a3e0c0e9da06f8
Chuong Hoang <hoangthienchuong2302@gmail.com>
2021-09-12 13:33:05 +0700

    feat(UI): Component Empty Screen

--- src/screens/Common/OnBoardScreen/index.js
+++ src/screens/Common/OnBoardScreen/index.js
@@ -1,6 +1,6 @@
 import React, {useCallback, useRef, useState} from 'react';
 
-import {Text} from '@components';
+import {EmptyScreen, Text} from '@components';
 import {theme} from '@theme';
 import {Dimensions, Image, View, TouchableOpacity} from 'react-native';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
@@ -30,16 +30,7 @@ const OnBoardScreen = () => {
 
   const _renderItem = useCallback(({item}) => {
     return (
-      <View style={styles.renderRoot}>
-        <Image
-          source={{
-            uri: item.img,
-          }}
-          style={styles.image}
-        />
-        <Text style={styles.renderTitle}>{item.title}</Text>
-        <Text style={styles.renderText}>{item.text}</Text>
-      </View>
+      <EmptyScreen picture={item.img} title={item.title} text={item.text} />
     );
   }, []);
 
