5c78e32f4c4303f0fdb077922ddded6b1e3ab721
jungleologyphong <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>
2021-10-26 21:21:52 +0700

    Feature/BMI

---
+++ src/api/bmiApi.js
@@ -0,0 +1,59 @@
+import {apiUrl} from '@config/api';
+import axios from 'axios';
+
+const getBMI = async (userId, options) => {
+  try {
+    const response = await axios.get(`${apiUrl}/bmi/${userId}`, {
+      ...options,
+      validateStatus: false,
+    });
+
+    if (response.status === 404 || response.status === 500) {
+      throw response.data;
+    }
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const addBMI = async (data, options) => {
+  try {
+    const response = await axios.post(`${apiUrl}/bmi/add`, data, {
+      ...options,
+      validateStatus: false,
+    });
+
+    if (response.status === 404 || response.status === 500) {
+      throw response.data;
+    }
+
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateBMI = async (data, options) => {
+  try {
+    const response = await axios.post(`${apiUrl}/bmi/update`, data, {
+      ...options,
+      validateStatus: false,
+    });
+
+    if (response.status === 404 || response.status === 500) {
+      throw response.data;
+    }
+
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const bmiApi = {
+  getBMI,
+  addBMI,
+  updateBMI,
+};
